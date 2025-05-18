712d5cd9eb45f36f9aff42d741ca8350b4a5dcac
ddmytro-m <marchuk.dmytro.v@gmail.com>
2025-05-18 09:11:54 +0300

    Subscription

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,7 @@ import router from "./routes"
 import express from "express"
 const app = express()
 
+app.use(express.json())
 app.use(router)
 
 app.get("/hello", (req, res) => {
---
+++ api/src/services/SubscriptionService.ts
@@ -0,0 +1,48 @@
+import { prisma } from "../prisma/prisma"
+import type { Subscription } from "../types/models/Subscription"
+
+import createHttpError from "http-errors"
+
+class SubscriptionService {
+  private async emailExists(email: string) {
+    return (await prisma.subscription.count({ where: { email } })) > 0
+  }
+
+  private async isTokenValid(token: string, confirmed: boolean) {
+    return await prisma.subscription.count({
+      where: { token, confirmed },
+    })
+  }
+
+  public async subscribe(data: Subscription) {
+    if (await this.emailExists(data.email)) throw createHttpError(409, "Email already subscribed")
+
+    const { token } = await prisma.subscription.create({
+      data: {
+        email: data.email,
+        city: data.city,
+        frequency: data.frequency,
+      },
+      select: { token: true },
+    })
+
+    return token
+  }
+
+  public async confirm(token: string) {
+    if (!(await this.isTokenValid(token, false))) throw createHttpError(404, "Token not found")
+
+    await prisma.subscription.update({
+      where: { token },
+      data: { confirmed: true },
+    })
+  }
+
+  public async unsubscribe(token: string) {
+    if (!(await this.isTokenValid(token, true))) throw createHttpError(404, "Token not found")
+
+    await prisma.subscription.delete({ where: { token } })
+  }
+}
+
+export const subscriptionService = new SubscriptionService()
