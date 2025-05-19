ef9e03c9e9a6832473c46c7581144dddb813783a
Dmytro Marchuk <=>
2025-05-19 21:46:42 +0300

    added api startup logging and fixed nginx config

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,15 +9,18 @@ const app = express()
 
 void (async () => {
   await mailService.load()
+  console.log("Mail service is ready")
 
   mailWorker.run()
   scheduleWorker.run()
 
   await scheduleQueue.upsertJobScheduler("hourly-emails", { pattern: "0 * * * *" }) // once a hour
   await scheduleQueue.upsertJobScheduler("daily-emails", { pattern: "0 9 * * *" }) // once a day at 09:00
+  console.log("Schedulers are ready")
 
   app.use(express.json())
   app.use(router)
 
   app.listen(3000)
+  console.log("The server is listening at port 3000")
 })()
